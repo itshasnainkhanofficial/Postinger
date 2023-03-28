e59041b869a7481a40942771459e2dccd758e264
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-03-28 07:57:19 +0500

    front end navigation completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Deshboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      React app
+    <>
+    <BrowserRouter>
+    <div className="container">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Deshboard />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </div>
+    </BrowserRouter>
+    </>
   )
 }
 
