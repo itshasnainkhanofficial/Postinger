7d3c09fe31038d4fd3eee33db61b2c241fa4f309
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-04-07 19:43:20 +0500

    added vite code to resolve vercel error

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default  ({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons/Bs']
+    }
+  }
 })
