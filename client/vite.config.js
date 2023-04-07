28ad14c5b0631e34a6960aeb196f485e65cd8ce8
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-04-07 20:19:57 +0500

    final deployement

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default  ({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/Bs']
-    }
-  }
 })
+
+
