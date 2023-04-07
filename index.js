060b14f23eeff1d0c5fdc128b704343140f8b67f
Hasnain Khan <hasnainkhankqs@gmail.com>
2023-04-07 19:13:49 +0500

    remove serve front end

--- index.js
+++ index.js
@@ -38,17 +38,17 @@ app.use("/api/post", postRoutes)
 
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, './client/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, './client/dist')));
   
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, './', 'client', 'dist', 'index.html')
-      )
-    );
-  } else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-  }
+//     app.get('*', (req, res) =>
+//       res.sendFile(
+//         path.resolve(__dirname, './', 'client', 'dist', 'index.html')
+//       )
+//     );
+//   } else {
+//     app.get('/', (req, res) => res.send('Please set to production'));
+//   }
 
 // error handler
 app.use(errorHandler) // this will override express default error handler which is throw new Error("some error")
